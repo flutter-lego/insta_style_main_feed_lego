659a8234322c18a871db0a707e6bd4299bea3813
june lee <melodysdreamj@gmail.com>
2024-07-17 14:00:34 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/insta_style_main_feed_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForInstaStyleMainFeedLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/material_app/insta_style_main_feed_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMaterialApp()
+// Widget coverInstaStyleMainFeedLego(BuildContext context, Widget widget) {
+//   return widget;
+// }
